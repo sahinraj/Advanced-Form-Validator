cb10c55a8cf3b3194ebff6b4b3e3a6705ddfb4a1
Sahin Raj <sahinraj87@gmail.com>
2024-12-06 23:18:12 -0600

    Init XCode New project

---
+++ Advanced-Form-Validator/Advanced-Form-Validator/Advanced_Form_ValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_Form_ValidatorApp.swift
+//  Advanced-Form-Validator
+//
+//  Created by sahin raj on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_Form_ValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
